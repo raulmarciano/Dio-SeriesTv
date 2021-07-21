07a3d0e81a44b108a0a39bb1cfcdeb417ae0b37a
RAUL <raul_marciano@outlook.com>
2021-07-21 12:03:36 -0300

    Criação do projeto console

---
+++ Dio-SeriesTv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dio_SeriesTv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
